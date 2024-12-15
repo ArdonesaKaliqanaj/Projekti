3e1aa6dfe567316bf62a372b8d50cc459965d40d
vleragjinovci <gjinovcivlera71@gmail.com>
2024-12-15 01:50:40 +0100

    kompjuterlaptop kategoria

---
+++ kompjuterlaptop.js
@@ -0,0 +1,7 @@
+// Add interactivity if needed, e.g., adding products to a cart
+document.querySelectorAll('.product-card button').forEach((button) => {
+    button.addEventListener('click', () => {
+      alert('Produkti u shtua në shportë!');
+    });
+  });
+  
